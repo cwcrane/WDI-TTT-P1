8968c3f1958ebd0833150f18d387b1c637c3b8b4
cwcrane <ccrane@excelsiorintegrated.com>
2015-10-12 22:18:37 -0400

    Added eventhandlers for all nine ttt boxes. Also added on to the handler function a second step, which updates the object ttt_board, with an X in the correct key.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //2. 'getWinner' function to determine if there a winner.
 //3. 'getWinner' will need two callback functions 'three_X' and 'three_O', to determine whether X or O have three in a row yet.
 //4. create eventhandler for clicks on any box; handler should set the corresponding value in the ttt-board object to x.
+//5. Game logic: who goes first. Alternate turns. Stop when there's a winner.
 
 
 //1. Define TTT board as an object.
@@ -21,11 +22,27 @@ var three_X = function(){};
 var three_O = function(){};
 var getWinner = function getWinner(three_X, three_O){};
 
-//4. create eventhandler for clicks on any box; handler should set the corresponding value in the ttt-board object to x.
+//4. handler that appends X to box clicked.
 var setX = function setX(){
-  ///set ttt_board value to X
+  //if div.box clicked is empty, append X, otherwise do nothing.
   var $this = $(this);
-  $this.append('X');
+  if ($this.html()!== 'X'){
+    $this.append('X');
+  };
+  //update object ttt_board once div box has been clicked.
+  //take div box number that was clicked, and update the correcpsonging key with the value of X or O.
+  //$this.attr('class') --> returns the value of the class, of the div that was clicked.
+  ttt_board[$this.attr('class')]='X';
+  console.log(ttt_board);
 };
 
-$(".box").on('click', setX);
+
+$(".one").on('click', setX);
+$(".two").on('click', setX);
+$(".three").on('click', setX);
+$(".four").on('click', setX);
+$(".five").on('click', setX);
+$(".six").on('click', setX);
+$(".seven").on('click', setX);
+$(".eight").on('click', setX);
+$(".nine").on('click', setX);
