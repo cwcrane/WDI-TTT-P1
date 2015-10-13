6ecf096fb7506feb4f7be7ebb8a2347ea965a2b5
cwcrane <ccrane@excelsiorintegrated.com>
2015-10-12 22:57:04 -0400

    Added function xAndO, which has a counter, and alternates between returning X and returning O. This in turn alternates eventhandler between appending X and appending O.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //1. Define TTT board as an object.
 //2. 'getWinner' function to determine if there a winner.
 //3. 'getWinner' will need two callback functions 'three_X' and 'three_O', to determine whether X or O have three in a row yet.
-//4. create eventhandler for clicks on any box; handler should set the corresponding value in the ttt-board object to x.
-//5. Game logic: who goes first. Alternate turns. Stop when there's a winner.
+//4. function to alternate between X and O.
+//5. create eventhandler for clicks on any box; handler should set the corresponding value in the ttt-board object to x.
+//6. Game logic: who goes first. Alternate turns. Stop when there's a winner.
 
 
 //1. Define TTT board as an object.
@@ -17,32 +18,41 @@ var ttt_board = {
   eight: null,
   nine: null,
 };
+
 //2-3. Functions to calculate winner.
 var three_X = function(){};
 var three_O = function(){};
 var getWinner = function getWinner(three_X, three_O){};
 
-//4. handler that appends X to box clicked.
-var setX = function setX(){
+//4. function to alternate between X and O.
+var counter = 0;
+var xAndO = function(){
+  if (counter%2===0){
+    return 'X';
+  }else return 'O';
+}
+
+//5. handler that appends X to box clicked.
+var setXO = function setXO(){
   //if div.box clicked is empty, append X, otherwise do nothing.
   var $this = $(this);
-  if ($this.html()!== 'X'){
-    $this.append('X');
+  if ($this.html()!== xAndO){
+    $this.append(xAndO);
   };
   //update object ttt_board once div box has been clicked.
-  //take div box number that was clicked, and update the correcpsonging key with the value of X or O.
   //$this.attr('class') --> returns the value of the class, of the div that was clicked.
-  ttt_board[$this.attr('class')]='X';
-  console.log(ttt_board);
+  ttt_board[$this.attr('class')]= xAndO;
+  //increment counter so that var xAndO will return a different value the next time it's called.
+  counter++;
 };
 
 
-$(".one").on('click', setX);
-$(".two").on('click', setX);
-$(".three").on('click', setX);
-$(".four").on('click', setX);
-$(".five").on('click', setX);
-$(".six").on('click', setX);
-$(".seven").on('click', setX);
-$(".eight").on('click', setX);
-$(".nine").on('click', setX);
+$(".one").on('click', setXO);
+$(".two").on('click', setXO);
+$(".three").on('click', setXO);
+$(".four").on('click', setXO);
+$(".five").on('click', setXO);
+$(".six").on('click', setXO);
+$(".seven").on('click', setXO);
+$(".eight").on('click', setXO);
+$(".nine").on('click', setXO);
